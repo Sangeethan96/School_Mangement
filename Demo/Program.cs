4dcd6ed4cc9f76fd83545825566cf17c71b80bff
Sangeethan96 <sangeethan96@gmail.com>
2024-03-30 11:22:55 +0530

    Swagger get edit

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -9,35 +9,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Demo;
 
-/*
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run(); */
-
-
-
  namespace Demo
 {
     public class Program
@@ -57,79 +28,4 @@ app.Run(); */
 
 
 }  
-/*
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json.Serialization;
-using Microsoft.OpenApi.Models;
-
-namespace Demo
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.ConfigureServices(services =>
-                    {
-                        // Enable CORS
-                        services.AddCors(options =>
-                        {
-                            options.AddPolicy("AllowOrigin", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-                        });
-
-                        // JSON Serializer
-                        services.AddControllersWithViews().AddNewtonsoftJson(options =>
-                        {
-                            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-                            options.SerializerSettings.ContractResolver = new DefaultContractResolver();
-                        });
-
-                        services.AddControllers();
-
-                        // Swagger configuration
-                        services.AddSwaggerGen(c =>
-                        {
-                            c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-                        });
-                    })
-                    .Configure(app =>
-                    {
-                        var env = app.ApplicationServices.GetRequiredService<IWebHostEnvironment>();
-
-                        if (env.IsDevelopment())
-                        {
-                            app.UseDeveloperExceptionPage();
-                        }
-
-                        // Enable CORS
-                        app.UseCors("AllowOrigin");
-
-                        // Swagger
-                        app.UseSwagger();
-                        app.UseSwaggerUI(c =>
-                        {
-                            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                        });
-
-                        app.UseRouting();
-                        app.UseAuthorization();
-
-                        app.UseEndpoints(endpoints =>
-                        {
-                            endpoints.MapControllers();
-                        });
-                    });
-                });
-    }
-} */
 
